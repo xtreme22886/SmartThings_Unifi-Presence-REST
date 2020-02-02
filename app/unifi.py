2098366399fc1318e9e5f89d559f082efdd350e5
xtreme22886 <erik.w.clay@gmail.com>
2020-02-02 11:57:28 -0800

    Added monitoring of hotspot clients

--- app/unifi.py
+++ app/unifi.py
@@ -37,10 +37,22 @@ def getConfig(): # Define getConfig() function
     logoutURL = '{}api/logout'.format(baseURL) # Define URL to use to log out of the UniFi Controller
     global knownClientsURL # Initilize global variable
     knownClientsURL = '{}api/s/{}/rest/user'.format(baseURL, siteID) # Define URL to use to get list of known clients from the UniFi Controller
+    global hotspotManagerURL # Initilize global variable
+    hotspotManagerURL = '{}api/s/{}/stat/guest'.format(baseURL, siteID) # Define URL to use to get list of known guest VIA the hotspot manager
     return settings # Return contents of 'config.json'
 
 getConfig() # Initilize config settings at bootup
 session = requests.Session() # Initilize request session at bootup
+
+# Ensure we are logged into the UniFi Controller and maintain an active session
+def sessionPersist(): # Define sessionPersist() function
+    check_session = session.get(loggedinURL, verify=False) # Query an API endpoint to see if we are logged in
+    if check_session.status_code == 200: # We are already logged in
+        return session # Return current session
+    elif check_session.status_code == 401: # If query response returns '401' (not logged in) then
+        login_response = session.post(loginURL, data=json.dumps(loginCreds).encode('utf8'), headers={'Content-type': 'application/json'}, verify=False) # Log into the UniFi API
+        if login_response.status_code == 200: # If login response was successful
+            return session # Return new session
 
 # Take a list of supplied mac address' and check their presence
 def CheckPresence(macList): # Define CheckPresence() function
@@ -54,20 +66,31 @@ def CheckPresence(macList): # Define CheckPresence() function
         try: # See if
             visibleToUAP = macStats['_last_seen_by_uap'] # We can pull a value from this key
             if macStats['is_wired'] ==  False: # If we can, and device is showing not wired
-                results.append({'mac': mac, 'present': True}) # Then, mark device as 'online'
+                results.append({'id': "unifi-" + mac[-5:], 'present': True}) # Then, mark device as 'online'
         except: # If not
-            results.append({'mac': mac, 'present': False}) # If so, mark device as 'offline'
+            # If macStats['is_wired'] == True: # Then check if Unifi shows device as wired
+            results.append({'id': "unifi-" + mac[-5:], 'present': False}) # If so, mark device as 'offline'
     return results # Return results list when done
 
-# Ensure we are logged into the UniFi Controller and maintain an active session
-def sessionPersist(): # Define sessionPersist() function
-    check_session = session.get(loggedinURL, verify=False) # Query an API endpoint to see if we are logged in
-    if check_session.status_code == 200: # We are already logged in
-        return session # Return current session
-    elif check_session.status_code == 401: # If query response returns '401' (not logged in) then
-        login_response = session.post(loginURL, data=json.dumps(loginCreds).encode('utf8'), headers={'Content-type': 'application/json'}, verify=False) # Log into the UniFi API
-        if login_response.status_code == 200: # If login response was successful
-            return session # Return new session
+def GuestCheckPresence(): # Define CheckPresence() function
+    session = sessionPersist() # Run sessionPersist() to ensure we are logged in
+    macList = HotSpotClients()
+    if macList:
+        results = ""
+        for mac in macList: # For each mac in macList
+            macStatsURL = '{}api/s/{}/stat/user/{}'.format(baseURL, siteID, mac) # Define URL to use to get details about given mac address
+            macStatsResponse = session.get(macStatsURL) # Request details of given mac address from the UniFi Controller
+            data = macStatsResponse.json().get('data') # Grab the 'data' from the reply
+            macStats = dict(data[0]) # Convert 'data' json list to dict (grab first element of list)
+            try: # See if
+                visibleToUAP = macStats['_last_seen_by_uap'] # We can pull a value from this key
+                if macStats['is_wired'] ==  False: # If we can, and device is showing not wired
+                    return {'id': 'unifi-guest', 'present': True} # Then, mark guest as 'online'
+            except: # If not
+                results = {'id': 'unifi-guest', 'present': False} # Mark guest as 'offline'
+        return results # Return results list when done
+    else:
+        return {'id': 'unifi-guest', 'present': False}
 
 # Generate a list of known UniFi clients
 def UniFiClients(): # Define UniFiClients() function
@@ -79,11 +102,6 @@ def UniFiClients(): # Define UniFiClients() function
         clientList = knownClientsResponse.json().get('data') # Grab the 'data' from the reply
         knownUniFiClients = [] # Initilize list
         for client in clientList: # For each client in clientList
-            if client.get('is_wired', None) != None: # Get client 'is_wired' value. If none found, use default 'None'. If 'is_wired' does not equal None then continue
-                if client['is_wired'] == False: # If client 'is_wired' equals False
-                    wireless = True # Then, set 'wireless' to True
-                else:
-                    wireless = False # Else, set 'wireless' to False
             if client.get('mac', None) != None: # Get client 'mac' value. If none found, use default 'None'. If 'mac' does not equal None then continue
                 mac = client['mac'] # Set 'mac' to clients mac
             if client.get('name', None) != None: # Get client 'name' value. If none found, use default 'None'. If 'name' does not equal None then continue
@@ -92,10 +110,23 @@ def UniFiClients(): # Define UniFiClients() function
                 name = client['hostname'] # Set name to clients hostname (perfer hostname over name)
             else:
                 name = "unknown" # Set name to 'unknown' if a name/hostname could not be found
-
-            ## Only add wireless clients to knownWifiClients list
-            #if wireless == True: # If client wireless equals True
             knownUniFiClients.append({'name': name + " (" + mac[-5:] + ")", 'mac': mac, 'id': "unifi-" + mac[-5:]}) # Append client information to knownUniFiClients list
         return sorted(knownUniFiClients, key = lambda i: i['name']) # Return a sorted (by name) list of konwn UniFi clients
+    else: # No config file
+        return # Return NUL
+
+def HotSpotClients():
+    # Ensure we have latest config settings
+    check = getConfig() # Load data from the config file (force a load here in case config settings have changed)
+    if check: # If config file exist
+        session = sessionPersist() # Run sessionPersist() to ensure we are logged in
+        hotspotManagerResponse = session.get(hotspotManagerURL) # Request list of known clients
+        guestList = hotspotManagerResponse.json().get('data') # Grab the 'data' from the reply
+        guestMacList = [] # Initilize list
+        for guest in guestList: # For each guest in guestList
+            if guest.get('expired') == False:
+                if guest.get('mac', None) != None: # Get client 'mac' value. If none found, use default 'None'. If 'mac' does not equal None then continue
+                    guestMacList.append(guest['mac']) # Append client information to knownUniFiClients list
+        return guestMacList # Return a sorted (by name) list of konwn UniFi clients
     else: # No config file
         return # Return NUL
